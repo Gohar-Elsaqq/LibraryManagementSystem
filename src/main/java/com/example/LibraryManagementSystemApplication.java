2c45098448d1d07a967fc342c76dc665c523bf57
Gohar-Elsaqq <gohar.elsaqqa@gmail.com>
2024-10-20 21:54:20 +0300

    ADD main LibraryManagementSystem

--- src/main/java/com/example/LibraryManagementSystemApplication.java
+++ src/main/java/com/example/LibraryManagementSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryManagementSystemApplication {
-	git add .
+
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementSystemApplication.class, args);
 	}
